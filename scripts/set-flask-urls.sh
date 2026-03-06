6f32fcc72bdeb3d1d213d9fdaf6128df40c9ac2d
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2026-03-06 10:55:31 +0000

    Remove unneeded echo of command

--- scripts/set-flask-urls.sh
+++ scripts/set-flask-urls.sh
@@ -7,6 +7,5 @@ for filename in $TARGET_HTML_DIR/*.html; do
     [ -e "$filename" ] || continue
     
     REPLACE_CMD='sed -i -E -e "s/URL_FOR:\/(\S+?)\"/{{url_for\('\''static'\'', filename='\''img\/\1'\'')}}\"/g" '$filename
-    echo "Executing command: $REPLACE_CMD"
     eval $REPLACE_CMD
 done
