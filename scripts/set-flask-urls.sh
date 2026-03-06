22eba9c6672880b462f4695287e0e3793aded6e9
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2026-03-06 10:47:43 +0000

    Set up script to turn URLs into Flask format

---
+++ scripts/set-flask-urls.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Look through all .html files in the static/html directory, and for any urls with the "URL_FOR:" prefix, replace the
+# URL with a properly wrapped "url_for" command for Flask
+
+for filename in $TARGET_HTML_DIR/*.html; do
+    [ -e "$filename" ] || continue
+    
+    REPLACE_CMD='sed -i -E -e "s/URL_FOR:\/(\S+?)\"/{{url_for\('\''static'\'', filename='\''img\/\1'\'')}}\"/g" '$filename
+    echo "Executing command: $REPLACE_CMD"
+    eval $REPLACE_CMD
+done
