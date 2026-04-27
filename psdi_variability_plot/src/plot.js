7545c1f848a2764710df5de39c5c95d39566a6eb
Don Cruickshank <dgc@soton.ac.uk>
2026-04-27 13:03:04 +0100

    Fixed issue with black overlay box.

--- psdi_variability_plot/src/plot.js
+++ psdi_variability_plot/src/plot.js
@@ -908,7 +908,7 @@ export class Chart {
         const boxOffsetY = 20;
 
         const boxBackground = this.createSVGElement("rect", {
-            class: "legendBackground",
+            stroke: "none",
             x: 0,
             y: 0,
             width: 0,
@@ -916,7 +916,7 @@ export class Chart {
         })
 
         const boxBorder = this.createSVGElement("rect", {
-            class: "legendBorder",
+            fill: "none",
             x: 0,
             y: 0,
             width: 0,
